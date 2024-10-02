c1d0ee6eae8918602a397f808b9e74e242986653
adityx-reposit <adityayadav2022@vitbhopal.ac.in>
2024-10-02 12:09:13 +0530

    week1

---
+++ index.js
@@ -0,0 +1,70 @@
+// let firstname="aditya";
+// const age =30;
+// var isStudent = true;
+// console.log(age);
+// let user =["adittya", "heloo","panga"];
+// console.log(user[1]);
+// function sum(a,b) {
+//     let sum =a+b;
+//     return sum;
+// }
+
+        
+// canVote(17);
+// **********************looping concept***********************
+// let user =["adittya", "heloo","panga","adityaaaa"];
+// for(let i=0;i<user.length;i++){
+//     console.log(user[i]);
+// }
+// *************************OBJECT*********************************
+// function greet(user1){
+//     console.log(" hi "+ user1.name + "your age is"+ user1.age);
+    
+// }
+// function canVote(age) {
+//     if(age>18){
+//         console.log("can vote");
+        
+//     }
+//     else{
+//         console.log("minor");
+//     }
+// }
+//     var user1={
+//     name: "aditya",
+//     age: 19,
+//     gender : "male"
+//     }
+//     greet(user1.name);
+//     canVote(user1.age);
+    
+// ****************** ARRAY OF OBJECTS **************************
+let na = ["aditya", 12,{
+    name: "aditya",
+    age: 190
+}];
+console.log(na[2]);
+
+function user(arr) {
+    if(users.gender=="male"&& users.age> 18){
+        console.log(users);   
+    }
+    else{
+        console.log("no one");
+        
+    }
+}
+const users=[{
+    name:"aditya",
+    age:19,
+    gender: "male"
+},{
+    name:"gohit",
+    age:13,
+    gender: "male"
+},{
+    name:"sam",
+    age:29,
+    gender: "female"
+}
+]
